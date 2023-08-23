a20cd2e83fc6804e09f0d69ae3ad4fb65d9443ac
samkelisiwe <samnkelisiwe.kaula.younglings.africa>
2023-08-23 12:35:26 +0200

    button

--- src/components/buttonsNoBack.jsx
+++ src/components/buttonsNoBack.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7e46b8d245269d067a75950dc3d0a059a51694b167c37fd10557bda9b677faa
-size 369
+oid sha256:a9614108a38ad64fec3814c8bb5bf8a77bdc7ec7f1fdf2f7d673037e42448885
+size 396
