589e64c1ca874259c78bec83fc7e18c6244a054f
samkelisiwe <samnkelisiwe.kaula.younglings.africa>
2023-08-31 13:06:25 +0200

    editing the about items

---
+++ src/components/AboutItem.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:47b1940594aa2825bea6e572ddadc8f15c9bbb6a8a85059eae9753c7be1cc692
+size 430
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d053cb28087b0da3fe59feef52ea0801fa6dd43912ac7b0003999cda30c78ef
-size 2878
+oid sha256:7de71d980e8e9580ef79b3a7881556b530c029708364a125edff7f7142b84163
+size 3235
